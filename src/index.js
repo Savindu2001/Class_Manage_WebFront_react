f049afe41fbf3d6f6c881991da4eb426b505880b
Savindu2001 <76149334+Savindu2001@users.noreply.github.com>
2024-02-09 21:35:40 +0530

    made home and login pages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
+
 import reportWebVitals from './reportWebVitals';
+import Login from './Pages/Login'
+import Home from './Pages/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+    <Home />
+    <Login />
+    
   </React.StrictMode>
 );
 
